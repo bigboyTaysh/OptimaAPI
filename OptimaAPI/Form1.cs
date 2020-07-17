db7285f0867be2167945f9db0f05e3e548fa2119
bigboyTaysh <pvtryk.w@gmail.com>
2020-07-17 09:42:28 +0200

    [change] - unlocking the application after a failed login attempt

--- OptimaAPI/Form1.cs
+++ OptimaAPI/Form1.cs
@@ -16,10 +16,10 @@ namespace OptimaAPI
         private bool dragging = false;
         private Point dragCursorPoint;
         private Point dragFormPoint;
-
         public Form1()
         {
             InitializeComponent();
+            System.Environment.CurrentDirectory = @"C:\Program Files (x86)\Comarch ERP Optima";
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
@@ -28,7 +28,6 @@ namespace OptimaAPI
             dragCursorPoint = Cursor.Position;
             dragFormPoint = this.Location;
         }
-
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
             if (dragging)
@@ -37,51 +36,52 @@ namespace OptimaAPI
                 this.Location = Point.Add(dragFormPoint, new Size(dif));
             }
         }
-
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
             dragging = false;
         }
-
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
                 button1.PerformClick();
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             System.Windows.Forms.Application.Exit();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             string loginText = textBox1.Text;
             string password = textBox2.Text;
             string company = textBox3.Text;
-
-            System.Environment.CurrentDirectory = @"C:\Program Files (x86)\Comarch ERP Optima";
+            ILogin login;
+            IApplication application = null;
 
             try
             {
-                IApplication application = new CDNBase.Application();
+                application = new CDNBase.Application();
+                login = application.LockApp(256, 5000, null, null, null, null);
+                login = application.Login(
+                    loginText,
+                    password,
+                    company,
+                    1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0);
                 
-                ILogin login = application.LockApp(256, 5000, null, null, null, null);
-
-                login = application.Login(loginText, password, company, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0);
-
                 this.Visible = false;
 
                 Form2 appForm = new Form2(application, login);
                 appForm.Show();
                 appForm.Activate();
 
-            } catch (System.Runtime.InteropServices.COMException ex) 
+            } catch (Exception ex) 
             {
+                if(application != null)
+                {
+                    application.UnlockApp();
+                }
                 label4.Text = "Błąd logowania";
             }
         }
-
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.DrawRectangle(new Pen(Color.Gray, 3),
